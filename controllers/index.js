d565cabc11e255c5120a67b9103cdb51653855a5
Paulo McNally <paulomcnally@gmail.com>
2014-12-20 04:13:41 -0600

    set post controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,42 @@
 'use strict';
 
-
 var IndexModel = require('../models/index');
 
+module.exports = function(router) {
 
-module.exports = function (router) {
+  var model = new IndexModel();
 
-    var model = new IndexModel();
+  router.get('/', function(req, res) {
 
+    res.render('index', {month:null});
 
-    router.get('/', function (req, res) {
-        
-        
-        res.render('index', model);
-        
-        
-    });
+  });
+
+  router.post('/', function(req, res) {
+
+    var modelSalary = model.get(req.body.salary);
+
+    var modelData = {
+      month: {
+        salary: modelSalary.getSalary(),
+        salaryFree: modelSalary.getSalaryFree(),
+        salaryAnnual: modelSalary.getAnnual(),
+        inss: modelSalary.getInss(),
+        ir: modelSalary.getIr(),
+        retention: modelSalary.getRetention()
+      },
+      fortnightly: {
+        salary: modelSalary.getSalaryFortnightly(),
+        salaryFree: modelSalary.getSalaryFreeFortnightly(),
+        salaryAnnual: modelSalary.getAnnual(),
+        inss: modelSalary.getInssFortnightly(),
+        ir: modelSalary.getIrFortnightly(),
+        retention: modelSalary.getRetentionFortnightly()
+      }
+    };
+
+    res.render('index', modelData);
+
+  });
 
 };
