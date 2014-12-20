c0efdbe3bfdafe194609acf7a74bf956ce4881b3
Paulo McNally <paulomcnally@gmail.com>
2014-12-20 04:22:02 -0600

    change port

--- server.js
+++ server.js
@@ -3,15 +3,14 @@
 var app = require('./index');
 var http = require('http');
 
-
 var server;
 
 /*
- * Create and start HTTP server.
- */
+* Create and start HTTP server.
+*/
 
 server = http.createServer(app);
-server.listen(process.env.PORT || 8000);
-server.on('listening', function () {
-    console.log('Server listening on http://localhost:%d', this.address().port);
+server.listen(process.env.PORT || 5000);
+server.on('listening', function() {
+  console.log('Server listening on http://localhost:%d', this.address().port);
 });
